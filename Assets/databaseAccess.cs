f1a1662ad38c69eb579691ce27bfe23d9ecd2c51
Seukachi <54280059+Seukachi@users.noreply.github.com>
2022-05-26 23:01:20 +0200

    init ProjectAlpha

---
+++ Assets/databaseAccess.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MongoDB.Driver;
+
+public class databaseAccess : MonoBehaviour
+{
+    private const string MONGO_URI = "mongodb+srv://test:test@projetalpha.pzbb4q9.mongodb.net/?retryWrites=true&w=majority";
+    private const string DATABASE_NAME = "test";
+    private MongoClient client;
+    private IMongoDatabase db;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        client = new MongoClient(MONGO_URI);
+        db = client.GetDatabase(DATABASE_NAME);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
